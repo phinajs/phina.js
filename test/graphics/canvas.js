8635fd4049d243c08b874a12e363a4220f890eb7
phi <phi1618jp@gmail.com>
2015-07-13 14:16:29 +0900

    fix test page

--- test/graphics/canvas.js
+++ test/graphics/canvas.js
@@ -1,4 +1,19 @@
-th.describe("canvas", function() {
+th.describe("graphics.Canvas", function() {
+
+  th.it('setSize', function() {
+    var canvas = phina.graphics.Canvas('#world');
+    canvas.setSize(256, 256);
+  });
+
+  th.it('setSizeToScreen', function() {
+    var canvas = phina.graphics.Canvas('#world');
+    canvas.setSizeToScreen();
+  });
+
+  th.it('fitScreen', function() {
+    var canvas = phina.graphics.Canvas('#world');
+    canvas.fitScreen();
+  });
 
   th.it('fillRect', function() {
     var canvas = phina.graphics.Canvas('#world');
@@ -7,7 +22,22 @@ th.describe("canvas", function() {
 
   th.it('fillStar', function() {
     var canvas = phina.graphics.Canvas('#world');
+    canvas.fillStyle = 'hsla(60, 90%, 50%, 1.0)';
     canvas.fillStar(100, 100);
+    canvas.fillStar(200, 100, 32);
+    canvas.fillStar(300, 100, 32, 10);
+    canvas.fillStar(400, 100, 32, 5, 0.2);
+    canvas.fillStar(500, 100, 32, 5, null, 90);
+  });
+
+  th.it('strokeStar', function() {
+    var canvas = phina.graphics.Canvas('#world');
+    canvas.strokeStyle = 'hsla(60, 90%, 0%, 1.0)';
+    canvas.strokeStar(100, 100);
+    canvas.strokeStar(200, 100, 32);
+    canvas.strokeStar(300, 100, 32, 10);
+    canvas.strokeStar(400, 100, 32, 5, 0.2);
+    canvas.strokeStar(500, 100, 32, 5, null, 90);
   });
 
 });
