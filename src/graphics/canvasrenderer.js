8e1c1d3380771e0080914600dfde56d48bff9f8a
phi <phi1618jp@gmail.com>
2015-08-18 13:36:06 +0900

    fix renderer

---
+++ src/graphics/canvasrenderer.js
@@ -0,0 +1,34 @@
+
+
+phina.namespace(function() {
+  
+  phina.define('phina.graphics.CanvasRenderer', {
+
+    init: function(canvas) {
+      this.canvas = canvas;
+      this._context = this.canvas.context;
+    },
+    render: function(scene) {
+      this.canvas.clear();
+      
+      this._context.save();
+      this.renderObject(scene);
+      this._context.restore();
+    },
+    renderObject: function(obj) {
+      if (obj.visible === false) return ;
+
+      if (obj.draw) {
+        obj.draw(this.canvas);
+      }
+      // 子供たちも実行
+      if (obj.children.length > 0) {
+          var tempChildren = obj.children.slice();
+          for (var i=0,len=tempChildren.length; i<len; ++i) {
+              this.renderObject(tempChildren[i]);
+          }
+      }
+    },
+  });
+
+});
