14c689103a06081c6784971f860b048e85cb6fc6
phi <phi1618jp@gmail.com>
2015-08-18 13:35:04 +0900

    fix object2d and scene

---
+++ src/app/object2d.js
@@ -0,0 +1,15 @@
+phina.namespace(function() {
+
+
+  phina.define('phina.app.Object2D', {
+    superClass: 'phina.app.Element',
+
+    position: null,
+
+    init: function() {
+      this.superInit();
+    },
+  });
+
+  
+});
---
+++ src/app/scene.js
@@ -0,0 +1,12 @@
+phina.namespace(function() {
+
+
+  phina.define('phina.app.Scene', {
+    superClass: 'phina.app.Element',
+
+    init: function() {
+      this.superInit();
+    },
+  });
+  
+});
