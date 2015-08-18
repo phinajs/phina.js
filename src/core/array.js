70e54bb79d9c3e83cabb383565e744494b155926
phi <phi1618jp@gmail.com>
2015-08-18 20:40:42 +0900

    fix core

---
+++ src/core/array.js
@@ -0,0 +1,21 @@
+
+;(function() {
+  Array.prototype.accessor("last", {
+    "get": function()   { return this[this.length-1]; },
+    "set": function(v)  { this[this.length-1] = v; }
+  });
+
+  Array.prototype.method("clone", function(deep) {
+    if (deep === true) {
+      var a = Array(this.length);
+      for (var i=0,len=this.length; i<len; ++i) {
+        a[i] = (this[i].clone) ? this[i].clone(deep) : this[i];
+      }
+      return a;
+    }
+    else {
+      return Array.prototype.slice.apply(this);
+    }
+  });
+
+})();
