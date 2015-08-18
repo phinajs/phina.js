fa09827e8c33b7e05ede1d896ce127e514d8c66a
phi <phi1618jp@gmail.com>
2015-08-18 13:24:08 +0900

    fix element

---
+++ src/app/element.js
@@ -0,0 +1,91 @@
+phina.namespace(function() {
+
+  phina.define('phina.app.Element', {
+    superClass: 'phina.event.EventDispatcher',
+
+    /// 親
+    parent: null,
+
+    /// 子供
+    children: null,
+
+    /**
+     * @constructor
+     */
+    init: function() {
+      this.superInit();
+      this.children = [];
+    },
+
+    addChild: function() {
+      if (child.parent) child.remove();
+
+      child.parent = this;
+      this.children.push(child);
+
+      child.has('added') && child.flare('added');
+
+      return child;
+    },
+
+    addChildTo: function(parent) {
+      parent.addChild(this);
+
+      return this;
+    },
+
+    addChildAt: function(child, index) {
+      if (child.parent) child.remove();
+
+      child.parent = this;
+      this.children.splice(index, 0, child);
+
+      child.has('added') && child.flare('added');
+
+      return child;
+    },
+
+    getChildAt: function(index) {
+      return this.children.at(index);
+    },
+
+    getChildByName: function(name) {
+      // TODO: 
+    },
+
+    getChildIndex: function(child) {
+      return this.children.indexOf(child);
+    },
+
+    getParent: function() {
+      return this.parent;
+    },
+
+    getRoot: function() {
+      var elm = this;
+      for (elm=this.parent; elm.parent != null; elm = elm.parent) {
+        
+      }
+      return elm;
+    },
+
+    removeChild: function(child) {
+      var index = this.children.indexOf(child);
+      if (index !== -1) {
+        this.children.splice(index, 1);
+        child.has('removed') && child.flare('removed');
+      }
+      return this;
+    },
+
+    remove: function() {
+      if (!this.parent) return ;
+
+      this.parent.removeChild(this);
+      this.parent = null;
+      
+      return this;
+    },
+  });
+  
+});
