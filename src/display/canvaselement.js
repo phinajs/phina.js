26f4ddb0904809522d939130bce719d63d247e60
phi <phi1618jp@gmail.com>
2015-08-18 15:06:16 +0900

    add canvas element

---
+++ src/display/canvaselement.js
@@ -0,0 +1,13 @@
+
+phina.namespace(function() {
+
+  phina.define('phina.display.CanvasElement', {
+    superClass: 'phina.app.Object2D',
+
+    init: function() {
+      this.superInit();
+    },
+  });
+
+});
+
