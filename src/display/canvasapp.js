0d5d69486317a36d4e551facc9843c81f494bc02
phi <phi1618jp@gmail.com>
2015-08-18 13:35:27 +0900

    fix canvas app

--- src/display/canvasapp.js
+++ src/display/canvasapp.js
@@ -22,6 +22,8 @@ phina.namespace(function() {
       this.prevTime = new Date();
       this.deltaTime = 0;
 
+      var fps = 1000/30;
+
       var _run = function() {
         // start
         var start = (new Date()).getTime();
@@ -32,7 +34,9 @@ phina.namespace(function() {
         // calculate progress time
         var progress = (new Date()).getTime() - start;
         // calculate next waiting time
-        var newDelay = self.timer.frameTime-progress;
+        // var newDelay = self.timer.frameTime-progress;
+        var newDelay = fps-progress;
+
 
         // set next running function
         setTimeout(_run, newDelay);
@@ -52,8 +56,25 @@ phina.namespace(function() {
       this.prevTime = now;
     },
 
-    _draw: function() {
+    _update: function() {
+      this.currentScene._update();
+    },
 
+    _draw: function() {
+      this.canvas.clear();
+
+      this.currentScene._render();
+
+      var c = this.currentScene.canvas;
+      this.canvas.context.drawImage(c.domElement, 0, 0, c.width, c.height);
+    },
+
+    _accessor: {
+      currentScene: {
+        "get": function()   { return this._scenes[0]; },
+        "set": function(v)  { this._scenes[0] = v; },
+      },
     },
 
   });
+});
