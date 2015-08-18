55288d31ecbdd86fa5827badd29980235c11c784
phi <phi1618jp@gmail.com>
2015-08-18 15:20:57 +0900

    refactor app

--- src/app/baseapp.js
+++ src/app/baseapp.js
@@ -15,11 +15,59 @@ phina.namespace(function() {
       this.updater = phina.app.Updater(this);
     },
 
+    run: function() {
+      var self = this;
+
+      this.startedTime = new Date();
+      this.prevTime = new Date();
+      this.deltaTime = 0;
+
+      var fps = 1000/30;
+
+      var _run = function() {
+        // start
+        var start = (new Date()).getTime();
+
+        // run
+        self._loop();
+
+        // calculate progress time
+        var progress = (new Date()).getTime() - start;
+        // calculate next waiting time
+        // var newDelay = self.timer.frameTime-progress;
+        var newDelay = fps-progress;
+
+
+        // set next running function
+        setTimeout(_run, newDelay);
+      };
+
+      _run();
+
+      return this;
+    },
+
+    _loop: function() {
+      this._update();
+      this._draw();
+
+      var now = new Date();
+      this.deltaTime = now - this.prevTime;
+      this.prevTime = now;
+    },
+
+
     _update: function() {
       if (this.awake) {
         this.updater.update(this.currentScene);
       }
     },
+
+    /**
+     * 描画用仮想関数
+     * @private
+     */
+    _draw: function() {},
 
     _accessor: {
       currentScene: {
--- src/display/canvasapp.js
+++ src/display/canvasapp.js
@@ -13,47 +13,6 @@ phina.namespace(function() {
       this.canvas = phina.graphics.Canvas(this.domElement);
     },
 
-    run: function() {
-      var self = this;
-
-      this.startedTime = new Date();
-      this.prevTime = new Date();
-      this.deltaTime = 0;
-
-      var fps = 1000/30;
-
-      var _run = function() {
-        // start
-        var start = (new Date()).getTime();
-
-        // run
-        self._loop();
-
-        // calculate progress time
-        var progress = (new Date()).getTime() - start;
-        // calculate next waiting time
-        // var newDelay = self.timer.frameTime-progress;
-        var newDelay = fps-progress;
-
-
-        // set next running function
-        setTimeout(_run, newDelay);
-      };
-
-      _run();
-
-      return this;
-    },
-
-    _loop: function() {
-      this._update();
-      this._draw();
-
-      var now = new Date();
-      this.deltaTime = now - this.prevTime;
-      this.prevTime = now;
-    },
-
     _draw: function() {
       this.canvas.clear();
 
