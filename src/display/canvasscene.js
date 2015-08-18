5e5737a6d8c0272e204279c58b292de3ded95c18
phi <phi1618jp@gmail.com>
2015-08-18 13:35:55 +0900

    fix canvasscene

--- src/display/canvasscene.js
+++ src/display/canvasscene.js
@@ -5,15 +5,19 @@ phina.namespace(function() {
    * @class
    */
   phina.define('phina.display.CanvasScene', {
-    // superClass: 'phina.app.Scene',
+    superClass: 'phina.app.Scene',
 
     init: function() {
-      // this.superInit();
+      this.superInit();
 
       this.canvas = phina.graphics.Canvas();
       this.renderer = phina.graphics.CanvasRenderer(this.canvas);
+    },
 
-      this.children = [];
+    _update: function() {
+      if (this.update) {
+        this.update();
+      }
     },
 
     _render: function() {
@@ -22,32 +26,5 @@ phina.namespace(function() {
 
   });
 
-
-  phina.define('phina.graphics.CanvasRenderer', {
-
-    init: function(canvas) {
-      this.canvas = canvas;
-      this._context = this.canvas.context;
-    },
-    render: function(scene) {
-      this._context.save();
-      this.renderObject(scene);
-      this._context.restore();
-    },
-    renderObject: function(obj) {
-      if (obj.visible === false) return ;
-
-      if (obj.draw) {
-        obj.draw(this.canvas);
-      }
-      // 子供たちも実行
-      if (obj.children.length > 0) {
-          var tempChildren = obj.children.slice();
-          for (var i=0,len=tempChildren.length; i<len; ++i) {
-              this.renderObject(tempChildren[i]);
-          }
-      }
-    },
-  });
 
 });
