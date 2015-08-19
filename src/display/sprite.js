09d09be02d91682e5eb00a836fb0b8a93c827fb0
phi <phi1618jp@gmail.com>
2015-08-19 17:12:58 +0900

    fix benchmark

--- src/display/sprite.js
+++ src/display/sprite.js
@@ -8,11 +8,21 @@ phina.namespace(function() {
   phina.define('phina.display.Sprite', {
     superClass: 'phina.display.CanvasElement',
 
-    init: function() {
+    init: function(image) {
       this.superInit();
+
+      this.image = image;
+      this.width = image.width;
+      this.height = image.height;
     },
 
     draw: function(canvas) {
+
+      // canvas.context.drawImage(this.image, 0, 0, this.image.width, this.image.height);
+      canvas.context.drawImage(this.image,
+        0, 0, this.image.width, this.image.height,
+        -this.width*this.origin.x, -this.height*this.origin.y, this.width, this.height
+        );
     },
   });
 
