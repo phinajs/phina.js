4d98afc9f5e6111b061c0028db720a150605b896
phi <phi1618jp@gmail.com>
2015-08-19 16:29:28 +0900

    fix sprite

---
+++ src/display/sprite.js
@@ -0,0 +1,20 @@
+
+phina.namespace(function() {
+
+  /**
+   * @class phina.display.Sprite
+   * 
+   */
+  phina.define('phina.display.Sprite', {
+    superClass: 'phina.display.CanvasElement',
+
+    init: function() {
+      this.superInit();
+    },
+
+    draw: function(canvas) {
+    },
+  });
+
+});
+
