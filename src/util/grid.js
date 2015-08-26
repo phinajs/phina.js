9a9fb8c584e9d23254950ace758e850fc23d952f
phi <phi1618jp@gmail.com>
2015-08-26 18:09:14 +0900

    add grid

---
+++ src/util/grid.js
@@ -0,0 +1,46 @@
+
+;(function() {
+
+  /**
+   * @class phina.util.Ticker
+   * tick management class
+   */
+  phina.define('phina.util.Grid', {
+
+    /** 幅 */
+    width: 640,
+    /** 列数 */
+    columns: 12,
+    /** ループ */
+    loop: false,
+
+    init: function() {
+      if (typeof arguments[0] === 'object') {
+        var param = arguments[0];
+        width = param.width || 640;
+        columns = param.columns || 12;
+        loop = param.loop || false;
+      }
+
+      this.width = width;
+      this.columns = columns;
+      this.loop = loop;
+      this.unitWidth = this.width/this.columns;
+    },
+
+    // スパン指定で値を取得(負数もok)
+    span: function(index) {
+      if (loop) {
+        index += this.columns;
+        index %= this.columns;
+      }
+      return this.unitWidth * index;
+    },
+
+    center: function() {
+      return this.width/2;
+    },
+
+  });
+
+})();
