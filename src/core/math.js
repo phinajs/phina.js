6031c4345f5b5f5e31bcfe76b92d3900295e5691
phi <phi1618jp@gmail.com>
2015-08-28 17:20:59 +0900

    fix math extends

---
+++ src/core/math.js
@@ -0,0 +1,75 @@
+/*
+ * math.js
+ */
+
+;(function() {
+    
+  /**
+   * @class global.Math
+   * Mathの拡張
+   */
+
+  
+  /**
+   * @property    DEG_TO_RAD
+   * Degree to Radian.
+   */
+  Math.DEG_TO_RAD = Math.PI/180;
+  
+  /**
+   * @property    RAD_TO_DEG
+   * Radian to Degree.
+   */
+  Math.RAD_TO_DEG = 180/Math.PI;
+  
+  /**
+   * @method
+   * Degree を Radian に変換
+   */
+  Math.degToRad = function(deg) {
+    return deg * Math.DEG_TO_RAD;
+  };
+  
+  /**
+   * @method
+   * Radian を Degree に変換
+   */
+  Math.radToDeg = function(rad) {
+    return rad * Math.RAD_TO_DEG;
+  };
+  
+
+  
+  /**
+   * @method clamp
+   * クランプ
+   */
+  Math.method("clamp", function(value, min, max) {
+    return (value < min) ? min : ( (value > max) ? max : value );
+  });
+  
+  /**
+   * @method inside
+   * min <= value <= max のとき true を返す
+   */
+  Math.method("inside", function(value, min, max) {
+    return (value >= min) && (value) <= max;
+  });
+  
+  /**
+   * @method rand
+   * ランダムな値を指定された範囲内で生成
+   */
+  Math.method("rand", function(min, max) {
+    return window.Math.floor( Math.random()*(max-min+1) ) + min;
+  });
+  
+  /**
+   * @method randf
+   * ランダムな値を指定された範囲内で生成
+   */
+  Math.method("randf", function(min, max) {
+    return window.Math.random()*(max-min)+min;
+  });
+    
+})();
