87a2b933e40b11c71817ebaa76de9c1c0f421f56
phi <phi1618jp@gmail.com>
2015-09-01 12:13:09 +0900

    fix game test

--- test/game/src/display/index.js
+++ test/game/src/display/index.js
@@ -1,17 +1,12 @@
 th.describe("display.Label", function() {
 
   th.it('init', function() {
-    var app = phina.display.CanvasApp({
-      query:'#world',
-    });
-    var scene = app.currentScene;
-    var label = phina.display.Label('Hello, world!').addChildTo(scene);
-    label.position.set(app.gridX.center(), app.gridY.center());
-    app.run();
+    var label = phina.display.Label('Hello, world!').addChildTo(this);
+    label.position.set(640/2, 960/2);
   });
 
   th.it('hoge', function() {
-    var label = phina.display.Label('Hello, world!').addChildTo(this);
+    var label = phina.display.Label('hoge!').addChildTo(this);
     label.position.set(640/2, 960/2);
   });
 
