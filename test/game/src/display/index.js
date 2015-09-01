7512e6b74f51014dc757bfedfdd571d0f0a5f17f
phi <phi1618jp@gmail.com>
2015-09-01 17:31:22 +0900

    fix three layer test

--- test/game/src/display/index.js
+++ test/game/src/display/index.js
@@ -99,3 +99,53 @@ th.describe("display.Label", function() {
   });
 
 });
+
+
+
+
+th.describe("display.ThreeLayer", function() {
+  th.it('sample', function() {
+    var SCREEN_WIDTH = 640;
+    var SCREEN_HEIGHT = 960;
+    var layer = phina.display.ThreeLayer({
+      width: SCREEN_WIDTH,
+      height: SCREEN_HEIGHT,
+    }).addChildTo(this);
+
+    var geometry = new THREE.BoxGeometry( 20, 20, 20 );
+
+    for ( var i = 0; i < 2000; i ++ ) {
+      var object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: Math.random() * 0xffffff } ) );
+
+      object.position.x = Math.random() * 800 - 400;
+      object.position.y = Math.random() * 800 - 400;
+      object.position.z = Math.random() * 800 - 400;
+
+      object.rotation.x = Math.random() * 2 * Math.PI;
+      object.rotation.y = Math.random() * 2 * Math.PI;
+      object.rotation.z = Math.random() * 2 * Math.PI;
+
+      object.scale.x = Math.random() + 0.5;
+      object.scale.y = Math.random() + 0.5;
+      object.scale.z = Math.random() + 0.5;
+
+      layer.scene.add( object );
+    }
+
+    var theta = 0;
+    var radius = 100;
+    layer.update = function() {
+      theta += 0.5;
+
+      this.camera.position.x = radius * Math.sin( THREE.Math.degToRad( theta ) );
+      this.camera.position.y = radius * Math.sin( THREE.Math.degToRad( theta ) );
+      this.camera.position.z = radius * Math.cos( THREE.Math.degToRad( theta ) );
+      this.camera.lookAt( this.scene.position );
+
+      this.camera.updateMatrixWorld();
+    }
+  });
+
+});
+
+
