c4c901236ab5ced07ea082b1c742d528f19dd33d
phi <phi1618jp@gmail.com>
2015-09-04 11:28:07 +0900

    fix scene

--- src/app/scene.js
+++ src/app/scene.js
@@ -7,6 +7,28 @@ phina.namespace(function() {
     init: function() {
       this.superInit();
     },
+
+    exit: function(param) {
+      if (!this.app) return ;
+
+      if (typeof param !== 'object') {
+        var temp = {};
+        temp.nextLabel = arguments[0];
+        temp.nextArguments = arguments[1];
+        param = temp;
+      }
+
+      if (param.nextLabel) {
+        this.nextLabel = param.nextLabel;
+      }
+      if (param.nextArguments) {
+        this.nextArguments = param.nextArguments;
+      }
+
+      this.app.popScene();
+
+      return this;
+    },
   });
   
 });
