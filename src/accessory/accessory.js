7eb37c82dfb4ede0eabbfe387143e61e39138bb4
phi <phi1618jp@gmail.com>
2015-09-04 16:57:11 +0900

    fix accessory

---
+++ src/accessory/accessory.js
@@ -0,0 +1,70 @@
+
+phina.namespace(function() {
+
+  /**
+   * @class phina.accessory.Accessory
+   */
+  phina.define('phina.accessory.Accessory', {
+    superClass: 'phina.util.EventDispatcher',
+
+    /**
+     * @constructor
+     */
+    init: function(target) {
+      this.superInit();
+
+      this.target = target;
+    },
+    setTarget: function(target) {
+      if (this.target === target) return ;
+
+      this.target = target;
+      return this;
+    },
+    getTarget: function() {
+      return this.target;
+    },
+    isAttached: function() {
+      return !!this.target;
+    },
+    attachTo: function(element) {
+      element.attach(this);
+      this.setTarget(element);
+      return this;
+    },
+    remove: function() {
+      this.target.detach(this);
+      this.target = null;
+    },
+  });
+
+  phina.app.Element.prototype.method('attach', function(accessory) {
+    if (!this.accessories) {
+      this.accessories = [];
+      this.on('enterframe', function(e) {
+        this.accessories.each(function(accessory) {
+          accessory.update && accessory.update(e.app);
+        });
+      });
+    }
+
+    this.accessories.push(accessory);
+    accessory.setTarget(this);
+    accessory.flare('attached');
+
+    return this;
+  });
+
+  phina.app.Element.prototype.method('detach', function(accessory) {
+    if (this.accessories) {
+      this.accessories.erase(accessory);
+      accessory.setTarget(null);
+      accessory.flare('detached');
+    }
+
+    return this;
+  });
+
+});
+
+
