763b46d4f44d8c6140fa9a65eb5351b753288129
phi <phi1618jp@gmail.com>
2015-09-08 13:08:49 +0900

    fix rect

--- src/geom/rect.js
+++ src/geom/rect.js
@@ -29,32 +29,124 @@ phina.namespace(function() {
       return this;
     },
 
+    moveTo: function(x, y) {
+      this.x = x;
+      this.y = y;
+      return this;
+    },
+
+    moveBy: function(x, y) {
+      this.x += x;
+      this.y += y;
+      return this;
+    },
+
+    setSize: function(w, h) {
+      this.width = w;
+      this.height = h;
+      return this;
+    },
+
+    padding: function(top, right, bottom, left) {
+      // css の padding に合わせて時計回りにパラメータ調整
+      switch (arguments.length) {
+        case 1:
+          top = right = bottom = left = arguments[0];
+          break;
+        case 2:
+          top     = bottom = arguments[0];
+          right   = left   = arguments[1];
+          break;
+        case 3:
+          top     = arguments[0];
+          right   = left = arguments[1];
+          bottom  = arguments[2];
+          break;
+      }
+      
+      this.x += left;
+      this.y += top;
+      this.width -= left+right;
+      this.height-= top +bottom;
+      
+      return this;
+    },
+
+    contains: function(x, y) {
+      return this.left <= x && x <= this.right && this.top <= y && y <= this.bottom;
+    },
+
     clone: function() {
       return phina.geom.Rect(this.x, this.y, this.width, this.height);
     },
 
     toCircle: function() {
-      return phina.geom.Circle()
+      var radius = ((this.width < this.height) ? this.width : this.height);
+      return phina.geom.Circle(this.centerX, this.centerY, radius);
+    },
+
+    /**
+     * 配列に変換
+     */
+    toArray: function() {
+      return [this.x, this.y, this.width, this.height];
     },
 
     _accessor: {
       
       /**
-       * @property  width
-       * width
+       * @property  left
+       * left
        */
-      width: {
-          "get": function()   { return this._width; },
-          "set": function(v)  { this._width = v; }
+      left: {
+        "get": function()   { return this.x; },
+        "set": function(v)  { this.width -= v-this.x; this.x = v; }
+      },
+      /**
+       * @property  top
+       * top
+       */
+      top: {
+        "get": function()   { return this.y; },
+        "set": function(v)  { this.height -= v-this.y; this.y = v; }
+      },
+      /**
+       * @property  right
+       * right
+       */
+      right: {
+        "get": function()   { return this.x + this.width; },
+        "set": function(v)  { this.width += v-this.right; },
+      },
+      /**
+       * @property  bottom
+       * bottom
+       */
+      bottom: {
+        "get": function()   { return this.y + this.height; },
+        "set": function(v)  { this.height += v-this.bottom; },
       },
       
       /**
-       * @property  height
-       * height
+       * @property  centerX
+       * centerX
        */
-      height: {
-          "get": function()   { return this._height; },
-          "set": function(v)  { this._height = v; }
+      centerX: {
+        "get": function()   { return this.x + this.width/2; },
+        "set": function(v)  {
+          // TODO: 検討中
+        },
+      },
+      
+      /**
+       * @property  centerY
+       * centerY
+       */
+      centerY: {
+        "get": function()   { return this.y + this.height/2; },
+        "set": function(v)  {
+          // TODO: 検討中
+        },
       },
     }
 
--- test/common/src/geom/index.js
+++ test/common/src/geom/index.js
@@ -70,6 +70,142 @@ describe('#geom', function() {
 
       assert.equal(r.x, 8);
       assert.equal(r.y, 16);
+      assert.equal(r.width, 32);
+      assert.equal(r.height, 64);
+    });
+
+    it('set', function() {
+      var rect  = phina.geom.Rect(1, 2, 3, 4);
+      rect.set(32, 64, 100, 200);
+
+      assert(rect.x === 32);
+      assert(rect.y === 64);
+      assert(rect.width  === 100);
+      assert(rect.height === 200);
+    });
+    
+    it('moveTo', function() {
+      var rect  = phina.geom.Rect(32, 64, 100, 200);
+      rect.moveTo(50, 100);
+
+      assert(rect.x === 50);
+      assert(rect.y === 100);
+      assert(rect.width  === 100);
+      assert(rect.height === 200);
+    });
+    
+    it('moveBy', function() {
+      var rect  = phina.geom.Rect(32, 64, 100, 200);
+      rect.moveBy(100, 200);
+
+      assert(rect.x === 132);
+      assert(rect.y === 264);
+      assert(rect.width  === 100);
+      assert(rect.height === 200);
+    });
+    
+    it('setSize', function() {
+      var rect  = phina.geom.Rect(32, 64, 100, 200);
+      rect.setSize(50, 100);
+
+      assert(rect.x === 32);
+      assert(rect.y === 64);
+      assert(rect.width  === 50);
+      assert(rect.height === 100);
+    });
+    
+    it('padding', function() {
+      var rect  = phina.geom.Rect(50, 100, 150, 200);
+
+      rect.set(50, 100, 150, 200);
+      rect.padding(10);
+      assert(rect.x === 60);
+      assert(rect.y ===110);
+      assert(rect.width  === 130);
+      assert(rect.height === 180);
+
+      rect.set(50, 100, 150, 200);
+      rect.padding(10, 20);
+      assert(rect.x === 70);
+      assert(rect.y ===110);
+      assert(rect.width  === 110);
+      assert(rect.height === 180);
+
+      rect.set(50, 100, 150, 200);
+      rect.padding(10, 20, 30);
+      assert(rect.x === 70);
+      assert(rect.y ===110);
+      assert(rect.width  === 110);
+      assert(rect.height === 160);
+
+      rect.set(50, 100, 150, 200);
+      rect.padding(10, 20, 30, 40);
+      assert(rect.x === 90);
+      assert(rect.y ===110);
+      assert(rect.width  === 90);
+      assert(rect.height === 160);
+    });
+    
+    it('contains', function() {
+      var rect  = phina.geom.Rect(32, 64, 128, 128);
+
+      assert.equal(rect.contains(35, 68), true);
+      assert.equal(rect.contains(200, 68), false);
+    });
+    
+    it('clone', function() {
+      var rect  = phina.geom.Rect(32, 64, 100, 200);
+      var rect2 = rect.clone();
+
+      assert(rect2.x === 32);
+      assert(rect2.y === 64);
+      assert(rect2.width  === 100);
+      assert(rect2.height === 200);
+    });
+
+    it('toCircle', function() {
+      var rect  = phina.geom.Rect(32, 64, 100, 200);
+      var circle = rect.toCircle();
+
+      assert(circle.x === 82);
+      assert(circle.y === 164);
+      assert(circle.radius  === 50);
+    });
+    
+    it('toArray', function() {
+      var rect  = phina.geom.Rect(32, 64, 100, 200);
+      var arr   = rect.toArray();
+
+      assert(arr.equals([32, 64, 100, 200]));
+    });
+
+    it('accessor', function() {
+      var rect = phina.geom.Rect(8, 16, 32, 64);
+
+      assert.equal(rect.left, 8);
+      assert.equal(rect.right, 40);
+      assert.equal(rect.top, 16);
+      assert.equal(rect.bottom, 80);
+
+      rect.left = 16;
+      assert.equal(rect.left, 16);
+      assert.equal(rect.right, 40);
+      assert.equal(rect.width, 24);
+
+      rect.right = 64;
+      assert.equal(rect.left, 16);
+      assert.equal(rect.right, 64);
+      assert.equal(rect.width, 48);
+
+      rect.top = 2;
+      assert.equal(rect.top, 2);
+      assert.equal(rect.bottom, 80);
+      assert.equal(rect.height, 78);
+
+      rect.bottom = 256;
+      assert.equal(rect.top, 2);
+      assert.equal(rect.bottom, 256);
+      assert.equal(rect.height, 254);
     });
   });
 
