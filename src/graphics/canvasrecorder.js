0723719820ce91e048616ea71f54fa808bbd7e07
phi <phi1618jp@gmail.com>
2015-09-08 18:17:45 +0900

    fix canvas recorder

---
+++ src/graphics/canvasrecorder.js
@@ -0,0 +1,55 @@
+/*
+ *
+ */
+
+
+phina.namespace(function() {
+  alert();
+
+  /**
+   * @class phina.graphics.CanvasRecorder
+   * 
+   */
+  phina.define('phina.graphics.CanvasRecorder', {
+
+    superClass: 'phina.util.EventDispatcher',
+
+    init: function(canvas) {
+      this.superInit();
+
+      this.canvas = canvas;
+
+      this.gif = new GIF({
+        workers: 4,
+        quality: 10,
+        width: canvas.width,
+        height: canvas.height,
+      });
+    },
+
+    start: function() {
+      var frameTime = 33;
+      var time = 0;
+      var id = setInterval(function() {
+        time+=frameTime;
+
+        console.log(time);
+
+        if (time > 3000) {
+          clearInterval(id);
+        }
+      }, frameTime);
+
+      return this;
+    },
+
+    stop: function() {
+
+    },
+
+    open: function() {
+
+    },
+  });
+
+});
