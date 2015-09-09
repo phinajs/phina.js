337a841e5045beb0ac97b5279b21aefa64d15009
phi <phi1618jp@gmail.com>
2015-09-09 16:50:51 +0900

    build

--- build/phina.js
+++ build/phina.js
@@ -5836,7 +5836,7 @@ phina.namespace(function() {
 
     init: function(text, style) {
 
-      this.text = text || 'hoge\nfoo\nbar';
+      this.text = text || 'hoge';
       style = (style || {}).$safe({
         color: 'black',
 
@@ -5887,7 +5887,7 @@ phina.namespace(function() {
 
       var fontSize = this.style.fontSize;
       var font = "{fontWeight} {fontSize}px {fontFamily}".format(this.style);
-      var lines = this.text.split('\n');
+      var lines = this._lines;
       canvas.context.font = font;
 
       canvas.width = this.calcWidth() + style.padding*2;
@@ -5930,7 +5930,7 @@ phina.namespace(function() {
         },
         set: function(v) {
           this._text = v;
-          this._lines = v.split('\n');
+          this._lines = (v+'').split('\n');
           if (this.canvas) {
             this._render();
           }
@@ -6745,7 +6745,7 @@ phina.namespace(function() {
           text: params.message,
           hashtags: params.hashtags,
         });
-        window.open(url);
+        window.open(url, 'share window', 'width=480, height=320');
       };
     },
 
@@ -6755,7 +6755,7 @@ phina.namespace(function() {
 
         message: 'this is phina.js project.\n',
         hashtags: 'phina,game,javascript',
-        url: location.href,
+        url: phina.global.location && phina.global.location.href,
 
         width: 640,
         height: 960,
@@ -6763,6 +6763,108 @@ phina.namespace(function() {
         fontColor: 'white',
         backgroundColor: 'hsl(200, 80%, 64%)',
         backgroundImage: '',
+      },
+    },
+
+  });
+
+});
+
+/*
+ * CountScene
+ */
+
+
+phina.namespace(function() {
+
+  /**
+   * @class phina.game.CountScene
+   * 
+   */
+  phina.define('phina.game.CountScene', {
+    superClass: 'phina.display.CanvasScene',
+    /**
+     * @constructor
+     */
+    init: function(options) {
+      this.superInit(options);
+
+      options = (options || {}).$safe(phina.game.CountScene.defaults);
+
+      this.backgroundColor = options.backgroundColor;
+
+      this.fromJSON({
+        children: {
+          label: {
+            className: 'phina.display.Label',
+            arguments: ['', {
+              color: 'white',
+              fontSize: options.fontSize,
+            }],
+            x: this.gridX.center(),
+            y: this.gridY.center(),
+          },
+        }
+      });
+
+      if (options.count instanceof Array) {
+        this.countList = options.count.reverse();
+      }
+      else {
+        this.countList = Array.range(1, options.count+1);
+      }
+      this.counter = this.countList.length;
+      this.exitType = options.exitType;
+
+      this._updateCount();
+    },
+
+    _updateCount: function() {
+      var endFlag = this.counter <= 0;
+      var index = --this.counter;
+
+      this.label.text = this.countList[index];
+
+      this.label.scale.set(1, 1);
+      this.label.tweener
+        .clear()
+        .to({
+          scaleX: 1,
+          scaleY: 1,
+          alpha: 1,
+        }, 250)
+        .wait(500)
+        .to({
+          scaleX: 1.5,
+          scaleY: 1.5,
+          alpha: 0.0
+        }, 250)
+        .call(function() {
+          if (this.counter <= 0) {
+            this.flare('finish');
+            if (this.exitType === 'auto') {
+              this.app.popScene();
+            }
+          }
+          else {
+            this._updateCount();
+          }
+        }, this);
+    },
+
+
+    _static: {
+      defaults: {
+        count: 3,
+
+        width: 640,
+        height: 960,
+
+        fontColor: 'white',
+        fontSize: 192,
+        backgroundColor: '#444',
+
+        exitType: 'auto',
       },
     },
 
@@ -6950,7 +7052,7 @@ phina.namespace(function() {
         screen_name: 'phi_jp',
         hashtags: 'javascript,phina',
         // url: 'http://github.com/phi-jp/phina.js',
-        url: location.href,
+        url: phina.global.location && phina.global.location.href,
         via: 'phi_jp',
       },
 
