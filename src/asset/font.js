d94a6b31cafb1dd33b4ca0c86bfe8a56eece2b1a
minimo <minimo.mobile@gmail.com>
2015-09-10 12:30:50 +0900

    add font.js

---
+++ src/asset/font.js
@@ -0,0 +1,68 @@
+phina.namespace(function() {
+  phina.define("phina.asset.Font", {
+    superClass: "phina.asset.Asset",
+
+    init: function() {
+      this.superInit();
+    },
+
+    load: function(key, path) {
+      this.src = path;
+      this.key = key;
+
+      var reg = /(.*)(?:\.([^.]+$))/;
+      var type = path.match(reg)[2];
+      var format = "unknown";
+      switch (type) {
+        case "ttf":
+            format = "truetype"; break;
+        case "otf":
+            format = "opentype"; break;
+        case "woff":
+            format = "woff"; break;
+        case "woff2":
+            format = "woff2"; break;
+      }
+
+      var text = "@font-face { font-family: '{0}'; src: url({1}) format('{2}'); }".format(key, path, format);
+      var e = document.querySelector("head");
+      var fontFaceStyleElement = document.createElement("style");
+      if (fontFaceStyleElement.innerText) {
+        fontFaceStyleElement.innerText = text;
+      } else {
+        fontFaceStyleElement.textContent = text;
+      }
+      e.appendChild(fontFaceStyleElement);
+
+      return phina.util.Flow(this._load.bind(this));
+    },
+
+    _load: function(resolve) {
+      var self = this;
+      this.checkLoaded(this.key, function() {
+        self.loaded = true;
+        resolve(self);
+      }.bind(this));
+    },
+
+    checkLoaded: function (font, callback) {
+      var canvas = phina.graphics.Canvas();
+      var DEFAULT_FONT = canvas.context.font.split(' ')[1];
+      canvas.context.font = '40px ' + DEFAULT_FONT;
+
+      var checkText = "1234567890-^\\qwertyuiop@[asdfghjkl;:]zxcvbnm,./\!\"#$%&'()=~|QWERTYUIOP`{ASDFGHJKL+*}ZXCVBNM<>?_１２３４５６７８９０－＾￥ｑｗｅｒｔｙｕｉｏｐａｓｄｆｇｈｊｋｌｚｘｃｖｂｎｍ，．あいうかさたなをん時は金なり";
+
+      var before = canvas.context.measureText(checkText).width;
+      canvas.context.font = '40px ' + font + ', ' + DEFAULT_FONT;
+
+      var checkLoadFont = function () {
+        if (canvas.context.measureText(checkText).width !== before) {
+          callback && callback();
+        } else {
+          setTimeout(checkLoadFont, 100);
+        }
+      };
+      setTimeout(checkLoadFont, 100);
+    },
+  });
+});
