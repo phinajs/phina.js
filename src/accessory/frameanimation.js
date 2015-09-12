deb769da9c7eb81e71016bc1548ba006062d6823
minimo <minimo.mobile@gmail.com>
2015-09-13 01:09:59 +0900

    add animation play finished flag.

--- src/accessory/frameanimation.js
+++ src/accessory/frameanimation.js
@@ -16,11 +16,18 @@ phina.namespace(function() {
 
       this.ss = phina.asset.AssetManager.get('spritesheet', ss);
       this.paused = true;
+      this.finished = false;
     },
 
     update: function() {
       if (this.paused) return ;
       if (!this.currentAnimation) return ;
+
+      if (this.finished) {
+        this.finished = false;
+        this.currentFrameIndex = 0;
+        return ;
+      }
 
       ++this.frame;
       if (this.frame%this.currentAnimation.frequency === 0) {
@@ -61,6 +68,7 @@ phina.namespace(function() {
           }
           else {
             this.pause = true;
+            this.finished = true;
             return ;
           }
         }
