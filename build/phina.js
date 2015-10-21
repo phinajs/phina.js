45ead2e3f2f8ee638cd3e8fdf5bd8184b2d8fed8
phi <phi1618jp@gmail.com>
2015-10-21 17:09:44 +0900

    build

--- build/phina.js
+++ build/phina.js
@@ -8382,7 +8382,7 @@ phina.namespace(function() {
      * @constructor
      */
     init: function(options) {
-      if (typeof arguments[0] === 'string') {
+      if (typeof arguments[0] !== 'object') {
         options = { text: arguments[0], };
       }
       else {
@@ -8444,7 +8444,8 @@ phina.namespace(function() {
 
       var fontSize = this.fontSize;
       var font = "{fontWeight} {fontSize}px {fontFamily}".format(this);
-      var lines = this._lines = this.text.split('\n');
+      var text = this.text + '';
+      var lines = this._lines = text.split('\n');
       canvas.context.font = font;
 
       var w = this.calcWidth() + this.padding*2;
