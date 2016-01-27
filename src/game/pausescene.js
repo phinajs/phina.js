1ee8f2e59c051fceb1d819b3281becce1be9b996
phi <phi1618jp@gmail.com>
2016-01-27 18:18:22 +0900

    add pause scene

---
+++ src/game/pausescene.js
@@ -0,0 +1,61 @@
+/*
+ * PauseScene
+ */
+
+
+phina.namespace(function() {
+
+  /**
+   * @class phina.game.PauseScene
+   *
+   */
+  phina.define('phina.game.PauseScene', {
+    superClass: 'phina.display.CanvasScene',
+    /**
+     * @constructor
+     */
+    init: function(params) {
+      this.superInit(params);
+
+      params = ({}).$safe(params, phina.game.PauseScene.defaults);
+
+      this.backgroundColor = params.backgroundColor;
+
+      this.fromJSON({
+        children: {
+          text: {
+            className: 'phina.display.Label',
+            arguments: {
+              text: 'Pause',
+              fill: params.fontColor,
+              stroke: null,
+              fontSize: 48,
+            },
+            x: this.gridX.center(),
+            y: this.gridY.center(),
+          },
+        }
+      });
+
+      if (params.exitType === 'touch') {
+        this.on('pointend', function() {
+          this.exit();
+        });
+      }
+    },
+
+    _static: {
+      defaults: {
+        width: 640,
+        height: 960,
+
+        fontColor: 'white',
+        backgroundColor: 'hsla(0, 0%, 0%, 0.85)',
+
+        exitType: 'touch',
+      },
+    },
+
+  });
+
+});
