11b2d84df4881a2e69f446d512ab478942ffef4d
simiraaaa <c7t2h8m3y0@yahoo.co.jp>
2016-02-23 01:49:43 +0900

    fix CircleGauge

--- src/ui/gauge.js
+++ src/ui/gauge.js
@@ -171,8 +171,6 @@ phina.namespace(function() {
     },
 
     render: function(canvas) {
-      canvas.clearColor(this.backgroundColor);
-      canvas.transformCenter();
       
       this.canvas.rotate(-Math.PI*0.5);
       this.canvas.scale(1, -1);
@@ -182,7 +180,7 @@ phina.namespace(function() {
       var startAngle = 0;
       var endAngle = end;
 
-      if (this.stroke) {
+      if (this.isStrokable()) {
         this.canvas.context.lineWidth = this.strokeWidth;
         this.canvas.strokeStyle = this.stroke;
         this.canvas.strokeArc(0, 0, this.radius, startAngle, endAngle);
