acc8f9cb484c8f5c43c6e54f03802357eed12e63
phi <phi1618jp@gmail.com>
2016-03-21 00:41:18 +0900

    remove pie timer

--- test/game/src/game.js
+++ test/game/src/game.js
@@ -119,32 +119,3 @@ th.describe('game.CountScene', function() {
 
 
 
-
-th.describe('game.PieTimer', function() {
-
-  th.it('default', function() {
-    var timer = phina.game.PieTimer().addChildTo(this);
-    timer.position.set(320, 480);
-  });
-
-  th.it('fill', function() {
-    var timer = phina.game.PieTimer(10*1000, {
-      radius: 128,
-      color: 'hsl(180, 70%, 60%)',
-      stroke: false,
-    }).addChildTo(this);
-    timer.position.set(320, 480);
-  });
-
-  th.it('stroke', function() {
-    var timer = phina.game.PieTimer(10*1000, {
-      radius: 128,
-      strokeWidth: 16,
-      color: 'transparent',
-    }).addChildTo(this);
-    timer.position.set(320, 480);
-  });
-
-});
-
-
