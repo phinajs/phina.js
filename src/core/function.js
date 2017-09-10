c1259cb1d1df7e5ef1b10a06ba72bff8e630494d
phi <phi1618jp@gmail.com>
2017-09-11 04:18:11 +0900

    fix function base

--- src/core/function.js
+++ src/core/function.js
@@ -24,4 +24,30 @@
     };
   });
 
+  /**
+   * @method timeout
+   */
+  Function.prototype.$method('timeout', function(time) {
+  });
+
+  /**
+   * @method loop
+   */
+  Function.prototype.$method('loop', function(time) {
+  });
+
+  /**
+   * @method throttle
+   */
+  Function.prototype.$method('throttle', function(time) {
+  });
+
+  /**
+   * @method debounce
+   */
+  Function.prototype.$method('debounce', function(time) {
+  });
+
 })();
+
+
